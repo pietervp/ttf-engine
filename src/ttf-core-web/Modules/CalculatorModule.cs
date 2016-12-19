a696e2815ed7508f72a23f66ee34a1406572472b
Pieter Van Parys <pieter.vanparys@gmail.com>
2016-12-19 10:10:35 +0100

    Update CalculatorModule.cs

--- src/ttf-core-web/Modules/CalculatorModule.cs
+++ src/ttf-core-web/Modules/CalculatorModule.cs
@@ -1,7 +1,6 @@
 using Nancy;
 using Ttf.Server.Core.Contracts;
 using Ttf.Server.Core.Engine;
-//using NLog;
 
 namespace Ttf.Server.Core.Web.Modules
 {
@@ -11,8 +10,6 @@ namespace Ttf.Server.Core.Web.Modules
         {
             Get("/{engine}/(?<options>[01]{3})", parameters =>
             {
-                //Logger.Debug("Start parsing request");
-
                 Input input;
                 CalculationEngine calculationEngine = GetEngine(parameters);
 
@@ -29,12 +26,8 @@ namespace Ttf.Server.Core.Web.Modules
                 if (!success)
                     return Error("Could not parse input");
 
-                //Logger.Debug($"Parsed input: {input}");
-
                 var result = calculationEngine.GetCollectionFor(input);
-
-                //Logger.Debug($"Calculated result: {result}");
-
+                
                 return Response.AsJson(result);
             });
         }
@@ -52,7 +45,6 @@ namespace Ttf.Server.Core.Web.Modules
 
         private Response Error(string msg)
         {
-            //Logger.Warn(msg);
             return Response.AsJson(new {Error = msg});
         }
     }
